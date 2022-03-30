bb6b77e5454347ab209189274d3ac0d956f25928
saikiran1224 <knvrssaikiran@gmail.com>
2022-03-30 11:55:17 +0530

    Added Android logo in the Card for ListWheelScrollView

--- lib/screens/main_screens/connect_screen.dart
+++ lib/screens/main_screens/connect_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/screens/main_screens/circulars_screen.dart';
 
 import '../../widgets/app_bar_title.dart';
 
@@ -33,22 +34,40 @@ class _ConnectScreenState extends State<ConnectScreen> {
                 itemExtent: 200,
                 diameterRatio: 2,
                 children: [
-                Container(
-                height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15)),
-                child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(
-                      Colors.greenAccent.withOpacity(0.2), BlendMode.dstATop),
-                  child: Center(
-                    child:  Image.asset(
-                      'assets/images/android_logo.png',
-                      height: 80,
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => CircularsScreen()));
+                  },
+                  child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Center(
+                        child: Image.asset(
+                            'assets/images/android_logo.png'
+                        ),
+
+                      )
+                  ),
+                ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => CircularsScreen()));
+                    },
+                    child: Container(
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Center(
+                          child: Image.asset(
+                              'assets/images/flutter_logo.png'
+                          ),
+
+                        )
                     ),
                   ),
-
-                )
-              ),
                   InkWell(
                     onTap: () {},
                     child: Container(
